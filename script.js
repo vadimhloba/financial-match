151a0100e6e1a301f251f3e9f0e9fea29706ae80
Yosuphew <vadimirua1409@gmail.com>
2022-10-18 17:55:47 +0300

    added adaptivity

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ range.addEventListener('input', e => {
 })
 // Input [type='range'] END
 
-
+// popup START
 const popupLink = document.querySelectorAll('.popup-link'),
 			popup = document.querySelector('.popup'),
 			closePopup = document.querySelectorAll('.close-popup'),
@@ -68,109 +68,4 @@ closePopup.forEach(el => {
 		popupText.innerHTML = ''
   })
 })
-
-
-/*
-
-// Popup START
-const popupLink = document.querySelectorALL('.popup-link')
-const body = document.querySelector('body')
-const lockPadding = document.querySelectorAll('.lock-padding')
-
-let unlock = true
-
-const timeout = 500
-
-if (popupLinks.length > 0) {
-	for (let index = 0; index < popupLinks.length; index++) {
-		const popupLink = popupLinks[index]
-		popupLink.addEventListener('click', e => {
-			const popupName = popupLink.getAttribute('href').replace('#', '')
-			const curentPopup = document.getElementsById(popupName)
-			popupOpen(curentPopup)
-			e.preventDefault()
-		})
-	}
-}
-
-const popupCloseIcon = document.querySelectorAll('.close-popup')
-if (popupCloseIcon.length > 0) {
-	for (let index = 0; index < popupCloseIcon.length; index++) {
-		const el = popupCloseIcon[index]
-		el.addEventListener('click', e => {
-			popupClose(el.closest('.popup'))
-			e.preventDefault()
-		})
-	}
-}
-
-function popupOpen(curentPopup) {
-	if (curentPopup && unlock) {
-		const popupActive = document.querySelector('.popup.open')
-		if (popupActive) {
-			popupClose(popupActive, false)
-		} else {
-			bodyLock()
-		}
-		curentPopup.classList.add('open')
-		curentPopup.addEventListener('click', e => {
-			if (!e.target.closest('.popup__content')) {
-				popupClose(e.target.closest('.popup'))
-			}
-		})
-	}
-}
-
-popupClose(popupActive, doUnlock = true); {
-	if (unlock) {
-		popupActive.classList.remove('open')
-		if (doUnlock) {
-			bodyUnlock()
-		}
-	}
-}
-
-function bodyLock() {
-	const lockPaddingValue = window.innerHTML - document.querySelector('wrapper').offsetWidth + 'px'
-
-	if (lockPadding.length > 0) {
-		for (let index = 0; index < lockPadding.length; index++) {
-			const el = lockPadding[index]
-			el.style.paddingRight = lockPaddingValue
-		}
-	}
-	body.style.paddingRight = lockPaddingValue
-	body.classList.add('lock')
-
-	unlock = false
-	setTimeout(function () {
-		unlock = true
-	}, timeout)
-}
-
-function bodyUnlock() {
-	setTimeout(function () {
-		if (lockPadding.length > 0) {
-			for (let index = 0; index < lockPadding.length; index++) {
-				const el = lockPadding[index]
-				el.style.paddingRight = '0px'
-			}
-		}
-		body.style.paddingRight = '0px'
-		body.classList.remove('lock')
-	}, timeout)
-	unlock = false
-	setTimeout(function () {
-		unlock = true
-	}, timeout)
-}
-
-document.addEventListener('keydown', function (e) {
-	if (e.which === 27) {
-		const popupActive = document.querySelector('.popup.open')
-		popupClose(popupActive)
-	}
-})
-// Popup END
-
-*/
+// popup END
